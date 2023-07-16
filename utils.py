fa7d04fc9e49017440837d6529c6238cedf933c2
Stanley Wei <32184969+stanley-wei@users.noreply.github.com>
2023-07-16 13:05:06 -0600

    Added requirements.txt
    Added requirements.txt
    Removed redundant functions from utils.py (namely get_rounded_multiple, dilate)

--- utils.py
+++ utils.py
@@ -6,12 +6,6 @@ import pyvista as pv
 
 import iostream
 
-def get_rounded_multiple(to_round, round_resolution):
-    if to_round % round_resolution > round_resolution / 2:
-        return int(to_round / round_resolution) + 1 if to_round > 0 else int(to_round / round_resolution)
-    else:
-        return int(to_round / round_resolution) if to_round >= 0 else int(to_round / round_resolution) - 1
-
 def find_coords_min_max(point_array):
     min_x = round(point_array[0][0], 2)
     max_x = round(point_array[0][0], 2)
@@ -57,8 +51,8 @@ def point_cloud_to_grid(point_cloud, resolution, resolution_z, min_maxes = []):
     arr = np.zeros([int(abs(max_y - min_y) / resolution) + 2, int(abs(max_x - min_x) / resolution) + 2])
 
     for i in range(point_cloud.shape[0]):
-        x = get_rounded_multiple(point_cloud[i][0] - min_x, resolution)
-        y = get_rounded_multiple(point_cloud[i][1] - min_y, resolution)
+        x = round((point_cloud[i][0] - min_x) / resolution)
+        y = round((point_cloud[i][1] - min_y) / resolution)
         z = point_cloud[i][2]
         if(z > arr[y][x]):
             arr[y][x] = z;
@@ -66,35 +60,14 @@ def point_cloud_to_grid(point_cloud, resolution, resolution_z, min_maxes = []):
     return arr;
 
 def grid_to_point_cloud(point_grid, resolution):
-    point_cloud = np.zeros((point_grid.shape[0] * point_grid.shape[1], 3), dtype = int);
+    point_cloud = np.zeros((0, 3), dtype = int);
+    to_delete = []
     for i in range(point_grid.shape[0]):
         for j in range(point_grid.shape[1]):
-            if math.isnan(point_grid[i][j]):
-                continue;
-            index = i * point_grid.shape[1] + j
-            point_cloud[index][0] = j * resolution;
-            point_cloud[index][1] = i * resolution;
-            point_cloud[index][2] = point_grid[i][j];
+            if not math.isnan(point_grid[i][j]):
+                to_append = [[j * resolution, i * resolution, point_grid[i][j]]]
+                point_cloud = np.append(point_cloud, to_append, axis=0)
     return point_cloud;
-
-def dilate(arr, to_dilate = 1, dilated = None, i = 0, j = 0):
-    if(dilated == None):
-        dilated = np.empty(shape = (0, 2), dtype = int);
-
-    if(arr[i][j] == to_dilate):
-        if(arr[i+1][j] == 0):
-            arr[i+1][j] = to_dilate;
-            dilated = np.append(dilated, [i+1, j]);
-
-        if(arr[i][j+1] == 0):
-            arr[i][j+1] = to_dilate;
-            dilated = np.append(dilated, [i, j+1]);
-
-    if(i < arr.shape[0]):
-        dilate(arr, dilated, i+1, j);
-
-    if(j < arr.shape[1]):
-        dilate(arr, to_dilate, dilated, i, j+1);
 
 def interpolate_holes(point_array):
     nonzeros = np.nonzero(point_array)
@@ -104,6 +77,7 @@ def interpolate_holes(point_array):
 
     holes = np.concatenate((np.where(np.isnan(point_array)), np.where(point_array == 0)), axis=1)
     if holes.any():
+        nonzeros = np.nonzero(point_array)
         point_array = interpolate.griddata(nonzeros, point_array[nonzeros], (grid_x, grid_y), method='nearest')
 
     return point_array
