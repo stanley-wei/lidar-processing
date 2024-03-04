2bb86cf37a8630ce383844309b409ca1c393bff6
Stanley Wei <32184969+stanley-wei@users.noreply.github.com>
2024-03-04 11:13:56 -0800

    Re-implemented U-Net model
    Implemented U-Net model from scratch
    Made additional adjustments to train.py

--- lidar-classification/model.py
+++ lidar-classification/model.py
@@ -1,129 +1,95 @@
-# Taken from: https://pyimagesearch.com/2021/11/08/u-net-training-image-segmentation-models-in-pytorch/
-# (with modification)
-
-# import the necessary packages
-import config
-from torch.nn import ConvTranspose2d
-from torch.nn import Conv2d
-from torch.nn import MaxPool2d
-from torch.nn import Module
-from torch.nn import ModuleList
-from torch.nn import ReLU
-from torchvision.transforms import CenterCrop
-from torch.nn import functional as F
+import numpy as np
 import torch
+import torch.functional as F
+from torchvision.transforms import CenterCrop
+import torchvision.transforms.functional as TF
 
-class Block(Module):
-    def __init__(self, inChannels, outChannels):
-        super().__init__()
+# Contains 2x Conv & Relu layers [+ an optional Batch Norm layer]
+class ConvRelu(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, normalize):
+        super(ConvRelu, self).__init__()
 
-        # store the convolution and RELU layers
-        self.conv1 = Conv2d(inChannels, outChannels, 3)
-        self.relu = ReLU()
-        self.conv2 = Conv2d(outChannels, outChannels, 3)
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.batch_norm = torch.nn.BatchNorm2d(out_channels)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, 3, padding=1)
+        self.activation = torch.nn.ReLU(inplace=True)
 
-    def forward(self, x):
-        # apply CONV => RELU => CONV block to the inputs and return it
-        return self.conv2(self.relu(self.conv1(x)))
-
-class Encoder(Module):
-    def __init__(self, channels=(1, 16, 32, 64)):
-        super().__init__()
-
-        # store the encoder blocks and maxpooling layer
-        self.encBlocks = ModuleList(
-            [Block(channels[i], channels[i + 1])
-                for i in range(len(channels) - 1)])
-        self.pool = MaxPool2d(2)
+        self.normalize = normalize
 
     def forward(self, x):
-        # initialize an empty list to store the intermediate outputs
-        blockOutputs = []
-
-        # loop through the encoder blocks
-        for block in self.encBlocks:
-            # pass the inputs through the current encoder block, store
-            # the outputs, and then apply maxpooling on the output
-            x = block(x)
-            blockOutputs.append(x)
-            x = self.pool(x)
-
-        # return the list containing the intermediate outputs
-        return blockOutputs
-
-class Decoder(Module):
-    def __init__(self, channels=(64, 32, 16)):
-        super().__init__()
-
-        # initialize the number of channels, upsampler blocks, and
-        # decoder blocks
-        self.channels = channels
-        self.upconvs = ModuleList(
-            [ConvTranspose2d(channels[i], channels[i + 1], 2, 2)
-                for i in range(len(channels) - 1)])
-        self.dec_blocks = ModuleList(
-            [Block(channels[i], channels[i + 1])
-                for i in range(len(channels) - 1)])
-
-    def forward(self, x, encFeatures):
-        # loop through the number of channels
-        for i in range(len(self.channels) - 1):
-            # pass the inputs through the upsampler blocks
-            x = self.upconvs[i](x)
-
-            # crop the current features from the encoder blocks,
-            # concatenate them with the current upsampled features,
-            # and pass the concatenated output through the current
-            # decoder block
-            encFeat = self.crop(encFeatures[i], x)
-            x = torch.cat([x, encFeat], dim=1)
-            x = self.dec_blocks[i](x)
-
-        # return the final decoder output
+        x = self.activation(self.conv1(x))
+        if self.normalize:
+            x = self.batch_norm(x)
+        x = self.activation(self.conv2(x))
         return x
 
-    def crop(self, encFeatures, x):
-        # grab the dimensions of the inputs, and crop the encoder
-        # features to match the dimensions
-        (_, _, H, W) = x.shape
-        encFeatures = CenterCrop([H, W])(encFeatures)
+# Runs a 2x Conv & Relu
+class EncoderBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(EncoderBlock, self).__init__()
 
-        # return the cropped features
-        return encFeatures
-
-class UNet(Module):
-    def __init__(self, encChannels=(1, 16, 32, 64),
-         decChannels=(64, 32, 16),
-         nbClasses=1, retainDim=True,
-         outSize=(config.INPUT_HEIGHT,  config.INPUT_WIDTH)):
-        super().__init__()
-
-        # initialize the encoder and decoder
-        self.encoder = Encoder(encChannels)
-        self.decoder = Decoder(decChannels)
-
-        # initialize the regression head and store the class variables
-        self.head = Conv2d(decChannels[-1], nbClasses, 1)
-        self.retainDim = retainDim
-        self.outSize = outSize
+        self.convRelu = ConvRelu(in_channels, out_channels, normalize=True)
 
     def forward(self, x):
-        # grab the features from the encoder
-        encFeatures = self.encoder(x)
+        x = self.convRelu(x)
+        return x
 
-        # pass the encoder features through decoder making sure that
-        # their dimensions are suited for concatenation
-        decFeatures = self.decoder(encFeatures[::-1][0],
-            encFeatures[::-1][1:])
+# Runs a Crop + 2x Conv & Relu
+class DecoderBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DecoderBlock, self).__init__()
 
-        # pass the decoder features through the regression head to
-        # obtain the segmentation mask
-        map = self.head(decFeatures)
+        self.convRelu = ConvRelu(in_channels, out_channels, normalize=False)
 
-        # check to see if we are retaining the original output
-        # dimensions and if so, then resize the output to match them
-        if self.retainDim:
-            map = F.interpolate(map, self.outSize)
-            
-        # return the segmentation map
-        return map
+    def forward(self, x1, x2):
+        # x = torch.cat((x1, CenterCrop(tuple(x1.shape[-2:]))(x2)), dim=1)
+        x = torch.cat((TF.resize(x1, x2.shape[-2:]), x2), dim=1)
+        x = self.convRelu(x)
+        return x
+
+class UNet(torch.nn.Module):
+
+    def __init__(self, num_classes, in_channels = 1):
+        super(UNet, self).__init__()
+
+        self.num_classes = num_classes if num_classes > 2 else 1
+        self.channels = [in_channels, 16, 32, 64]
+
+        # Encoding layers (down)
+        self.encoder_blocks = [EncoderBlock(self.channels[i], self.channels[i+1])
+            for i in range(len(self.channels)-1)]
+        self.max_pool = torch.nn.MaxPool2d(2, 2)
+
+        # Decoding layers (up)
+        self.up_convs = [torch.nn.ConvTranspose2d(self.channels[i], self.channels[i-1], 2, stride=2)
+            for i in range(len(self.channels)-1, 1, -1)]
+        self.decoder_blocks = [DecoderBlock(self.channels[i], self.channels[i-1])
+            for i in range(len(self.channels)-1, 1, -1)]
+
+        self.output_conv = torch.nn.Conv2d(self.channels[1], self.num_classes, 1)
+
+
+    def forward(self, x):
+        # TODO: Implement as loop based on # blocks in encoder_blocks
+        # Encoding phase
+        x1 = self.encoder_blocks[0](x)
+
+        x2 = self.max_pool(x1)
+        x2 = self.encoder_blocks[1](x2)
+
+        x3 = self.max_pool(x2)
+        x3 = self.encoder_blocks[2](x3)
+
+        # Decoding phase
+        x = self.up_convs[0](x3)
+        x = self.decoder_blocks[0](x, x2)
+
+        x = self.up_convs[1](x)
+        x = self.decoder_blocks[1](x, x1)
+
+        x = self.output_conv(x)
+
+        if self.num_classes <= 2:
+            return np.squeeze(x)
+        else:
+            return x
--- lidar-classification/train.py
+++ lidar-classification/train.py
@@ -1,5 +1,6 @@
 import albumentations
 from albumentations.pytorch import ToTensorV2
+import numpy as np
 import os
 from segmentation_models_pytorch.losses import DiceLoss
 from sklearn.model_selection import train_test_split
@@ -12,11 +13,8 @@ from tqdm import tqdm
 
 import config
 from dataset import SegmentationDataset
-#from aerial import UNet
 from model import UNet
-#from utils import EarlyStopper
 
-import numpy as np
 
 class EarlyStopper:
     def __init__(self, patience=1, min_delta=0):
@@ -78,7 +76,6 @@ def test_loop(dataloader, model, loss_fn):
             pred = model(X)
             test_loss += loss_fn(pred, y)
 
-    print(f"Test Error: Avg loss: {test_loss / num_batches:>8f} \n")
     return test_loss / test_steps
 
 if __name__ == "__main__":
@@ -90,11 +87,9 @@ if __name__ == "__main__":
     (trainImages, testImages) = split[:2]
     (trainMasks, testMasks) = split[2:]
 
-    # input_transforms = albumentations.Compose([albumentations.HorizontalFlip(p=0.5),
-    #     albumentations.VerticalFlip(p=0.5),
-    #     albumentations.RandomCrop(height=config.INPUT_HEIGHT,
-    #         width=config.INPUT_WIDTH)])
-    input_transforms = albumentations.Compose([albumentations.CenterCrop(height=config.INPUT_HEIGHT,
+    input_transforms = albumentations.Compose([albumentations.HorizontalFlip(p=0.5),
+        albumentations.VerticalFlip(p=0.5),
+        albumentations.RandomCrop(height=config.INPUT_HEIGHT,
             width=config.INPUT_WIDTH)])
 
     train_dataset = SegmentationDataset(imagePaths=trainImages, maskPaths=trainMasks,
@@ -113,38 +108,37 @@ if __name__ == "__main__":
         batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
         num_workers=os.cpu_count())
 
-    #model = UNet(n_classes = len(train_dataset.classes), n_channels=1)
-    model = UNet(nbClasses = len(train_dataset.classes))
-    #model = UNet(num_classes = len(train_dataset.classes), in_channels=1).to(config.DEVICE)
+    model = UNet(num_classes = len(train_dataset.classes), in_channels = 1)
 
-    #loss_fn = nn.CrossEntropyLoss()    
-    loss_fn = DiceLoss(mode='multiclass', classes=range(len(train_dataset.classes)))
+    if len(train_dataset.classes) <= 2:
+        loss_fn = nn.BCEWithLogitsLoss()
+    else:
+        loss_fn = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=config.LEARN_RATE)
 
     train_steps = len(train_dataset) // config.BATCH_SIZE
     test_steps = len(test_dataset) // config.BATCH_SIZE
 
-    epoch = 0
+    starting_epoch = 0
     early_stopper = EarlyStopper(patience=3, min_delta=0.5)
 
     if os.path.exists(config.CHECKPOINT_PATH) and config.USE_CHECKPOINT:
         checkpoint = torch.load(config.CHECKPOINT_PATH)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epoch = checkpoint['epoch']
+        starting_epoch = checkpoint['epoch']
 
-        print(f"Found checkpoint at {config.CHECKPOINT_PATH}; resuming from epoch {epoch+1}")
+        early_stopper.min_validation_loss = checkpoint['min_loss']
+        early_stopper.counter = checkpoint['current_patience']
 
-    while epoch < config.NUM_EPOCHS:
-        print(f"Epoch {epoch+1}\n-------------------------------")
-        
-        print("Training")
+        print(f"Found checkpoint at {config.CHECKPOINT_PATH}; resuming from epoch {starting_epoch+1}")
+
+    for epoch in range(starting_epoch, config.NUM_EPOCHS):
+        print(f"\nEpoch {epoch+1}/{config.NUM_EPOCHS}\n-------------------------------")
+
         train_loss = train_loop(train_dataloader, model, loss_fn, optimizer)
 
-        print("Testing")
         test_loss = test_loop(test_dataloader, model, loss_fn)
-
-        epoch += 1
 
         print("Train loss: {:.6f}, Test loss: {:.4f}".format(
             train_loss, test_loss))
@@ -153,6 +147,8 @@ if __name__ == "__main__":
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
+            'min_loss': early_stopper.min_validation_loss,
+            'current_patience': early_stopper.counter,
             }, config.CHECKPOINT_PATH)
         torch.save(model, config.MODEL_PATH[:-4] + "_checkpoint.pth")
 
@@ -161,10 +157,20 @@ if __name__ == "__main__":
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
+            'min_loss': early_stopper.min_validation_loss,
+            'current_patience': early_stopper.counter,
             }, config.CHECKPOINT_PATH[:-4]+f"_{epoch}.pth")
 
-        if early_stopper.early_stop(test_loss):             
+        if early_stopper.early_stop(test_loss):
             break
+        elif test_loss == early_stopper.min_validation_loss:
+            torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'min_loss': early_stopper.min_validation_loss,
+            'current_patience': early_stopper.counter,
+            }, config.CHECKPOINT_PATH[:-4]+f"_best_{early_stopper.min_validation_loss}.pth")
 
     print("Done!")
 
